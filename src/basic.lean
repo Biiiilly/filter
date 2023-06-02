2252f9a17227722178904d0bba04318bc802bd5b
Biiiilly <bm221@ic.ac.uk>
2023-06-02 01:06:22 +0100

    def of filter

--- src/basic.lean
+++ src/basic.lean
@@ -1,1 +1,13 @@
-example: 1 + 1 = 2 := sorry
+import tactic
+
+structure filter (X : Type*) :=
+(sets                     : set (set X))
+(upward_closure {F₁ F₂}   : F₁ ∈ sets → F₁ ⊆ F₂ → F₂ ∈ sets)
+(inter_sets {F₁ F₂}       : F₁ ∈ sets → F₂ ∈ sets → F₁ ∩ F₂ ∈ sets)
+
+instance {X : Type*}: has_mem (set X) (filter X) := ⟨λ U F, U ∈ F.sets⟩
+
+namespace filter
+
+
+end filter
